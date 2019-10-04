1a73330a8dd731f5943a59046f2f83b22efc8bde
aiden shin <heech1013@gmail.com>
2019-10-04 10:00:30 +0900

    Complete: [my profile] - [medicine] menu

--- back_end/routes/user/medicine/index.js
+++ back_end/routes/user/medicine/index.js
@@ -30,19 +30,19 @@ const index = async (req, res, next) => {
       const { id } = obj;
       const medicineName = obj.RegisteredMedicinePurposeData.nameKr;  // 이름 혹은 null
       const purposeOfPrescription = obj.UsedMedicineForDiagnosis ? obj.UsedMedicineForDiagnosis.nameKr
-                                      : obj.UsedMedicineForSymptom ? obj.UsedMedicineForSymptom.nameKr : null;
-      const perceivedEffect = !obj.perceivedEffectiveness ? null
+                                      : obj.UsedMedicineForSymptom ? obj.UsedMedicineForSymptom.nameKr : '-';
+      const perceivedEffect = !obj.perceivedEffectiveness ? '-'
                                 : obj.perceivedEffectiveness = 1 ? '알 수 없다'
                                   : obj.perceivedEffectiveness = 2 ? '없다'
                                     : obj.perceivedEffectiveness = 3 ? '약간'
                                       : obj.perceivedEffectiveness = 4 ? '보통'
                                         : obj.perceivedEffectiveness = 5 ? '크다' : '정보를 받아올 수 없습니다. 서버 관리자에게 문의해주세요.';
-      const degreeOfSideEffect = !obj.RegisteredMedicinePurposeData.RegisteredMedicineEvaluationData.sideEffects ? null
+      const degreeOfSideEffect = !obj.RegisteredMedicinePurposeData.RegisteredMedicineEvaluationData.sideEffects ? '-'
                                   : obj.RegisteredMedicinePurposeData.RegisteredMedicineEvaluationData.sideEffects = 1 ? '없다'
                                     : obj.RegisteredMedicinePurposeData.RegisteredMedicineEvaluationData.sideEffects = 2 ? '약간'
                                       : obj.RegisteredMedicinePurposeData.RegisteredMedicineEvaluationData.sideEffects = 3 ? '중간'
                                         : obj.RegisteredMedicinePurposeData.RegisteredMedicineEvaluationData.sideEffects = 4 ? '심각' : '정보를 받아올 수 없습니다. 서버 관리자에게 문의해주세요.';
-      const symptomOfSideEffect = obj.RegisteredMedicinePurposeData.RegisteredMedicineSideEffectsData ? obj.RegisteredMedicinePurposeData.RegisteredMedicineSideEffectsData[0].SymptomOfSideEffects.nameKr : null;
+      const symptomOfSideEffect = obj.RegisteredMedicinePurposeData.RegisteredMedicineSideEffectsData ? obj.RegisteredMedicinePurposeData.RegisteredMedicineSideEffectsData[0].SymptomOfSideEffects.nameKr + ' 등' : '-';
 
       return {
         id,
--- front_end/src/pages/profile/ProfileMedicinePage.js
+++ front_end/src/pages/profile/ProfileMedicinePage.js
@@ -4,11 +4,14 @@ import PageTemplate from 'components/common/PageTemplate';
 import MiniMenu from 'components/common/MiniMenu';
 import ProfileContentListContainer from 'containers/profile/ProfileContentListContainer';
 
-const ProfileMedicinePage = () => {
+const ProfileMedicinePage = ({match}) => {
+  const { id } = match.params;  // App.js에서 설정한 params 변수명: id
   return (
     <PageTemplate>
       <MiniMenu buttonArr={['프로필', '진단명', '증상', '처방약']}/>
-      <ProfileContentListContainer/>
+      <ProfileContentListContainer
+        type={'medicine'}
+        userId={id}/>
     </PageTemplate>
   )
 };
