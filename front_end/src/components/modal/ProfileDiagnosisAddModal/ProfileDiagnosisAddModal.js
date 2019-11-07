51413cf1e93da4e0328f0120ca82244863d6d37b
aiden shin <heech1013@gmail.com>
2019-11-07 23:34:36 +0900

    Processing: ProfileDiagnosisAddModal - year select options

--- front_end/src/components/modal/ProfileDiagnosisAddModal/ProfileDiagnosisAddModal.js
+++ front_end/src/components/modal/ProfileDiagnosisAddModal/ProfileDiagnosisAddModal.js
@@ -7,12 +7,25 @@ import Button from 'components/common/Button';
 const cx = classNames.bind(styles);
 
 class ProfileDiagnosisAddModal extends Component {
+  state = {
 
+  }
+
+
+  
   render() {
     const { visible } = this.props;
+    let yearSelectList = "";
+    for (let i = new Date().getFullYear(); i > 1909; i--) {
+      yearSelectList += ("<option value='", i, "'>", i, "</option>");
+      // 문제: 맨 마지막 단락만 추가가 됨 + 따옴표로 인해 STRING으로  들어감.
+    }
+
     return (
       <ModalWrapper visible={visible}>
-        
+        <div className={cx('form')}>
+          <select>{yearSelectList}</select>
+        </div>
       </ModalWrapper>
     )
   }
