b56fd9c53c5feb01e68799c8a3cff4e83c224c39
aiden shin <heech1013@gmail.com>
2019-11-14 20:51:02 +0900

    Processing: profileDiagnosisAddModal - diagnosis option list

--- front_end/src/components/modal/ProfileDiagnosisAddModal/ProfileDiagnosisAddModal.js
+++ front_end/src/components/modal/ProfileDiagnosisAddModal/ProfileDiagnosisAddModal.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import styles from './ProfileDiagnosisAddModal.scss';
 import classNames from 'classnames/bind';
 import ModalWrapper from 'components/modal/ModalWrapper';
-import Button from 'components/common/Button';
+// import Button from 'components/common/Button';
 
 const cx = classNames.bind(styles);
 
@@ -11,20 +11,28 @@ class ProfileDiagnosisAddModal extends Component {
 
   }
 
-
+  /** 추가 가능한 진단명 불러오기
+   * api 부른다
+   * store에 저장한다
+   *    content store 그대로 쓸 수 있나? 있겠다.
+   * prop으로 받아온다
+   * 배열? 객체? 등으로 받아와서 map이나 foreach 등. select option으로 세우면 될 듯??
+   */
   
   render() {
-    const { visible } = this.props;
-    let yearSelectList = "";
-    for (let i = new Date().getFullYear(); i > 1909; i--) {
-      yearSelectList += ("<option value='" + i + "'>" + i + "</option>");
-      // 문제: 맨 마지막 단락만 추가가 됨 + 따옴표로 인해 STRING으로  들어감.
-    }
+    const { visible, onCancel } = this.props;  // onCancle 전달해줘야 함
 
     return (
       <ModalWrapper visible={visible}>
         <div className={cx('form')}>
-          <select>{yearSelectList}</select>
+          <div className={cx('close')} onClick={() => onCancel()}>&times;</div>
+          <div>처음으로 해당 진단명과 관련된 증상을 알아차린 것이 언제인가요?</div>
+          <div>년</div>
+          <input name="firstNoticeYear" type="number" min="1900" max={new Date().getFullYear()} step="1" value="" />
+          <div>월 (알고 있다면 기입해주세요)</div>
+          <input name="firstNoticeMonth" type="number" min="1" max="12" step="1" value="" />
+          <div>일 (알고 있다면 기입해주세요)</div>
+          <input name="firstNoticeDay" type="number" min="1" max="31" step="1" value="" />
         </div>
       </ModalWrapper>
     )
