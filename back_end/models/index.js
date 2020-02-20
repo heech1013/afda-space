094e11b547a4799b44158f93514514a9b99d9e74
aiden shin <heech1013@gmail.com>
2020-02-20 16:57:49 +0900

    Processing: showDiagnosisSummary - done for ageArr

--- back_end/models/index.js
+++ back_end/models/index.js
@@ -33,115 +33,115 @@ db.MedicineEvaluationData = require('./MedicineEvaluationData')(sequelize, Seque
 db.MedicineSideEffectsData = require('./MedicineSideEffectsData')(sequelize, Sequelize);
 
 /* User:Profile = 1:1 */
-db.User.hasOne(db.Profile, { as: 'Profile', foreignKey: 'fkUserId' });
-db.Profile.belongsTo(db.User, { as: 'Profile', foreignKey: 'fkUserId' });
+db.User.hasOne(db.Profile, { foreignKey: 'fkUserId' });
+db.Profile.belongsTo(db.User, { foreignKey: 'fkUserId' });
 /* (Follow) User:User = N:M */
 db.User.belongsToMany(db.User, { through: 'Follow', as: 'Followers', foreignKey: 'followingId' });
 db.User.belongsToMany(db.User, { through: 'Follow', as: 'Followings', foreignKey: 'followerId' });
 
 /* User:Post = 1:N */
-db.User.hasMany(db.Post, { as: 'Posts', foreignKey: 'fkUserId' });
-db.Post.belongsTo(db.User, { as: 'Posts', foreignKey: 'fkUserId' });
+db.User.hasMany(db.Post, { foreignKey: 'fkUserId' });
+db.Post.belongsTo(db.User, { foreignKey: 'fkUserId' });
 /* Post:PostComment = 1:N */
-db.Post.hasMany(db.PostComment, { as: 'RegisteredPostComments', foreignKey: 'fkPostId' });
-db.PostComment.belongsTo(db.Post, { as: 'RegisteredPostComments', foreignKey: 'fkPostId' });
+db.Post.hasMany(db.PostComment, { foreignKey: 'fkPostId' });
+db.PostComment.belongsTo(db.Post, { foreignKey: 'fkPostId' });
 /* User:PostComment = 1:N */
-db.User.hasMany(db.PostComment, { as: 'RegisteringPostComments', foreignKey: 'fkUserId' });
-db.PostComment.belongsTo(db.User, { as: 'RegisteringPostComments', foreignKey: 'fkUserId' });
+db.User.hasMany(db.PostComment, { foreignKey: 'fkUserId' });
+db.PostComment.belongsTo(db.User, { foreignKey: 'fkUserId' });
 /* (PostBadge) User:Post = N:M */
 db.User.belongsToMany(db.Post, { through: 'PostBadge', as: '', foreignKey: '' });
 db.Post.belongsToMany(db.User, { through: 'PostBadge', as: '', foreignKey: '' });
 
 /* User:Center = 1:N */
-db.User.hasMany(db.Center, { as: 'RegisteringCenter', foreignKey: 'fkUserId' });
-db.Center.belongsTo(db.User, { as: 'RegisteringCenter', foreignKey: 'fkUserId' });
+db.User.hasMany(db.Center, { foreignKey: 'fkUserId' });
+db.Center.belongsTo(db.User, { foreignKey: 'fkUserId' });
 /* User:CenterComment = 1:N */
-db.User.hasMany(db.CenterComment, { as: 'RegisteringCenterComment', foreignKey: 'fkUserId' });
-db.CenterComment.belongsTo(db.User, { as: 'RegisteringCenterComment', foreignKey: 'fkUserId' });
+db.User.hasMany(db.CenterComment, { foreignKey: 'fkUserId' });
+db.CenterComment.belongsTo(db.User, { foreignKey: 'fkUserId' });
 /* Center:CenterComment = 1:N */
-db.Center.hasMany(db.CenterComment, { as: 'RegisteredCenterComment', foreignKey: 'fkCenterId' });
-db.CenterComment.belongsTo(db.Center, { as: 'RegisteredCenterComment', foreignKey: 'fkCenterId' });
+db.Center.hasMany(db.CenterComment, { foreignKey: 'fkCenterId' });
+db.CenterComment.belongsTo(db.Center, { foreignKey: 'fkCenterId' });
 /* (CenterBadge) User:CenterBadge = N:M */
 db.User.belongsToMany(db.Center, { through: 'CenterBadge', as: '', foreignKey: '' });
 db.Center.belongsToMany(db.User, { through: 'CenterBadge', as: '', foreignKey: '' });
 
 /* User:Station = 1:N */
-db.User.hasMany(db.Station, { as: 'RegisteringStation', foreignKey: 'fkUserId' });
-db.Station.belongsTo(db.User, { as: 'RegisteringStation', foreignKey: 'fkUserId' });
+db.User.hasMany(db.Station, { foreignKey: 'fkUserId' });
+db.Station.belongsTo(db.User, { foreignKey: 'fkUserId' });
 /* User:StationComment = 1:N */
-db.User.hasMany(db.StationComment, { as: 'RegisteringStationComment', foreignKey: 'fkUserId' });
-db.StationComment.belongsTo(db.User, { as: 'RegisteringStationComment', foreignKey: 'fkUserId' });
+db.User.hasMany(db.StationComment, { foreignKey: 'fkUserId' });
+db.StationComment.belongsTo(db.User, { foreignKey: 'fkUserId' });
 /* Station:StationComment = 1:N */
-db.Station.hasMany(db.StationComment, { as: 'RegisteredStationComment', foreignKey: 'fkStationId' });
-db.StationComment.belongsTo(db.Station, { as: 'RegisteredStationComment', foreignKey: 'fkStationId' });
+db.Station.hasMany(db.StationComment, { foreignKey: 'fkStationId' });
+db.StationComment.belongsTo(db.Station, { foreignKey: 'fkStationId' });
 /* (StationBadge) User:StationBadge = N:M */
 db.User.belongsToMany(db.Station, { through: 'StationBadge', as: '', foreignKey: '' });
 db.Station.belongsToMany(db.User, { through: 'StationBadge', as: '', foreignKey: '' });
 
 /* Diagnosis:DiagnosisData = 1:N */
-db.Diagnosis.hasMany(db.DiagnosisData, { as: 'RegisteredDiagnosisData', foreignKey: 'fkDiagnosisId' });
-db.DiagnosisData.belongsTo(db.Diagnosis, { as: 'RegisteredDiagnosisData', foreignKey: 'fkDiagnosisId' });
+db.Diagnosis.hasMany(db.DiagnosisData, { foreignKey: 'fkDiagnosisId' });
+db.DiagnosisData.belongsTo(db.Diagnosis, { foreignKey: 'fkDiagnosisId' });
 /* User:DiagnosisData = 1:N */
-db.User.hasMany(db.DiagnosisData, { as: 'RegisteringDiagnosisData', foreignKey: 'fkUserId' });
-db.DiagnosisData.belongsTo(db.User, { as: 'RegisteringDiagnosisData', foreignKey: 'fkUserId' });
+db.User.hasMany(db.DiagnosisData, { foreignKey: 'fkUserId' });
+db.DiagnosisData.belongsTo(db.User, { foreignKey: 'fkUserId' });
 
 /* Symptom:SymptomData = 1:N */
-db.Symptom.hasMany(db.SymptomData, { as: 'RegisteredSymptomData', foreignKey: 'fkSymptomId' });
-db.SymptomData.belongsTo(db.Symptom, { as: 'RegisteredSymptomData', foreignKey: 'fkSymptomId' });
+db.Symptom.hasMany(db.SymptomData, { foreignKey: 'fkSymptomId' });
+db.SymptomData.belongsTo(db.Symptom, { foreignKey: 'fkSymptomId' });
 /* User:SymptomData = 1:N */
-db.User.hasMany(db.SymptomData, { as: 'RegisteringSymptomData', foreignKey: 'fkUserId' });
-db.SymptomData.belongsTo(db.User, { as: 'RegisteringSymptomData', foreignKey: 'fkUserId' });
+db.User.hasMany(db.SymptomData, { foreignKey: 'fkUserId' });
+db.SymptomData.belongsTo(db.User, { foreignKey: 'fkUserId' });
 
 /* Medicine:MedicineData = 1:N */
-db.Medicine.hasMany(db.MedicineData, { as: 'RegisteredMedicineData', foreignKey: 'fkMedicineId' });
-db.MedicineData.belongsTo(db.Medicine, { as: 'RegisteredMedicineData', foreignKey: 'fkMedicineId' });
+db.Medicine.hasMany(db.MedicineData, { foreignKey: 'fkMedicineId' });
+db.MedicineData.belongsTo(db.Medicine, { foreignKey: 'fkMedicineId' });
 /* User:MedicineData = 1:N */
-db.User.hasMany(db.MedicineData, { as: 'RegisteringMedicineData', foreignKey: 'fkUserId' });
-db.MedicineData.belongsTo(db.User, { as: 'RegisteringMedicineData', foreignKey: 'fkUserId' });
+db.User.hasMany(db.MedicineData, { foreignKey: 'fkUserId' });
+db.MedicineData.belongsTo(db.User, { foreignKey: 'fkUserId' });
 
 /* Medicine:MedicineDosageData = 1:N */
-db.Medicine.hasMany(db.MedicineDosageData, { as: 'RegisteredMedicineDosageData', foreignKey: 'fkMedicineId' });
-db.MedicineDosageData.belongsTo(db.Medicine, { as: 'RegisteredMedicineDosageData', foreignKey: 'fkMedicineId' });
+db.Medicine.hasMany(db.MedicineDosageData, { foreignKey: 'fkMedicineId' });
+db.MedicineDosageData.belongsTo(db.Medicine, { foreignKey: 'fkMedicineId' });
 /* User:MedicineDosageData = 1:N */
-db.User.hasMany(db.MedicineDosageData, { as: 'RegisteringMedicineDosageData', foreignKey: 'fkUserId' });
-db.MedicineDosageData.belongsTo(db.User, { as: 'RegisteringMedicineDosageData', foreignKey: 'fkUserId' });
+db.User.hasMany(db.MedicineDosageData, { foreignKey: 'fkUserId' });
+db.MedicineDosageData.belongsTo(db.User, { foreignKey: 'fkUserId' });
 
 /** ReasonOfStop:MedicineDosageData = 1:1 */
-db.MedicineDosageData.hasOne(db.ReasonOfStop, { as: 'ReasonOfStop', foreignKey: 'fkMedicineDosageDataId'});
-db.ReasonOfStop.belongsTo(db.MedicineDosageData, { as: 'ReasonOfStop', foreignKey: 'fkMedicineDosageDataId'});
+db.MedicineDosageData.hasOne(db.ReasonOfStop, { foreignKey: 'fkMedicineDosageDataId'});
+db.ReasonOfStop.belongsTo(db.MedicineDosageData, { foreignKey: 'fkMedicineDosageDataId'});
 
 /* Medicine:MedicinePurposeData = 1:N */
 /** 하나의 처방약이 여러 개의 진단명/증상에 대해 처방될 수 있는 것이 최종 설계이지만 mvp에서는 기능상 처방 목적을 하나만 등록할 수 있다.
  * 다만 최종 설계로의 확장 용이성을 위해 일대다로 설계한다.
  */
-db.Medicine.hasMany(db.MedicinePurposeData, { as: 'RegisteredMedicinePurposeData', foreignKey: 'fkMedicineId' });
-db.MedicinePurposeData.belongsTo(db.Medicine, { as: 'RegisteredMedicinePurposeData', foreignKey: 'fkMedicineId' });
+db.Medicine.hasMany(db.MedicinePurposeData, { foreignKey: 'fkMedicineId' });
+db.MedicinePurposeData.belongsTo(db.Medicine, { foreignKey: 'fkMedicineId' });
 /* Diagnosis:MedicinePurposeData = 1:N */
-db.Diagnosis.hasMany(db.MedicinePurposeData, { as: 'UsedMedicineForDiagnosis', foreignKey: 'fkDiagnosisId' });
-db.MedicinePurposeData.belongsTo(db.Diagnosis, { as: 'UsedMedicineForDiagnosis', foreignKey: 'fkDiagnosisId' });
+db.Diagnosis.hasMany(db.MedicinePurposeData, { foreignKey: 'fkDiagnosisId' });
+db.MedicinePurposeData.belongsTo(db.Diagnosis, { foreignKey: 'fkDiagnosisId' });
 /* Symptom:MedicinePurposeData = 1:N */
-db.Symptom.hasMany(db.MedicinePurposeData, { as: 'UsedMedicineForSymptom', foreignKey: 'fkSymptomId' });
-db.MedicinePurposeData.belongsTo(db.Symptom, { as: 'UsedMedicineForSymptom', foreignKey: 'fkSymptomId' });
+db.Symptom.hasMany(db.MedicinePurposeData, { foreignKey: 'fkSymptomId' });
+db.MedicinePurposeData.belongsTo(db.Symptom, { foreignKey: 'fkSymptomId' });
 /* User:MedicinePurposeData = 1:N */
-db.User.hasMany(db.MedicinePurposeData, { as: 'RegisteringMedicinePurposeData', foreignKey: 'fkUserId' });
-db.MedicinePurposeData.belongsTo(db.User, { as: 'RegisteringMedicinePurposeData', foreignKey: 'fkUserId' });
+db.User.hasMany(db.MedicinePurposeData, { foreignKey: 'fkUserId' });
+db.MedicinePurposeData.belongsTo(db.User, { foreignKey: 'fkUserId' });
 
 /* Medicine:MedicineEvaluationData = 1:N */
-db.Medicine.hasMany(db.MedicineEvaluationData, { as: 'RegisteredMedicineEvaluationData', foreignKey: 'fkMedicineId' });
-db.MedicineEvaluationData.belongsTo(db.Medicine, { as: 'RegisteredMedicineEvaluationData', foreignKey: 'fkMedicineId' });
+db.Medicine.hasMany(db.MedicineEvaluationData, { foreignKey: 'fkMedicineId' });
+db.MedicineEvaluationData.belongsTo(db.Medicine, { foreignKey: 'fkMedicineId' });
 /* User:MedicineEvaluationData = 1:N */
-db.User.hasMany(db.MedicineEvaluationData, { as: 'RegisteringMedicineEvaluationData', foreignKey: 'fkUserId' });
-db.MedicineEvaluationData.belongsTo(db.User, { as: 'RegisteringMedicineEvaluationData', foreignKey: 'fkUserId' });
+db.User.hasMany(db.MedicineEvaluationData, { foreignKey: 'fkUserId' });
+db.MedicineEvaluationData.belongsTo(db.User, { foreignKey: 'fkUserId' });
 
 /* Medicine:MedicineSideEffectsData = 1:N */
-db.Medicine.hasMany(db.MedicineSideEffectsData, { as: 'RegisteredMedicineSideEffectsData', foreignKey: 'fkMedicineId' });
-db.MedicineSideEffectsData.belongsTo(db.Medicine, { as: 'RegisteredMedicineSideEffectsData', foreignKey: 'fkMedicineId' });
+db.Medicine.hasMany(db.MedicineSideEffectsData, { foreignKey: 'fkMedicineId' });
+db.MedicineSideEffectsData.belongsTo(db.Medicine, { foreignKey: 'fkMedicineId' });
 /* Symptom:MedicineSideEffectsData = 1:N */
-db.Symptom.hasMany(db.MedicineSideEffectsData, { as: 'SymptomOfSideEffects', foreignKey: 'fkSymptomId' });
-db.MedicineSideEffectsData.belongsTo(db.Symptom, { as: 'SymptomOfSideEffects', foreignKey: 'fkSymptomId' });
+db.Symptom.hasMany(db.MedicineSideEffectsData, { foreignKey: 'fkSymptomId' });
+db.MedicineSideEffectsData.belongsTo(db.Symptom, { foreignKey: 'fkSymptomId' });
 /* User:MedicineSideEffectsData = 1:N */
-db.User.hasMany(db.MedicineSideEffectsData, { as: 'RegisteringMedicineSideEffectsData', foreignKey: 'fkUserId' });
-db.MedicineSideEffectsData.belongsTo(db.User, { as: 'RegisteringMedicineSideEffectsData', foreignKey: 'fkUserId' });
+db.User.hasMany(db.MedicineSideEffectsData, { foreignKey: 'fkUserId' });
+db.MedicineSideEffectsData.belongsTo(db.User, { foreignKey: 'fkUserId' });
 
 
 module.exports = db;
--- back_end/routes/chart/showDiagnosisSummary.js
+++ back_end/routes/chart/showDiagnosisSummary.js
@@ -16,11 +16,10 @@ const showDiagnosisSummary = async (req, res, next) => {
       where: { id: diagnosisId },
       attributes: [
         'nameKr',
-        [Sequelize.fn("COUNT", Sequelize.col("RegisteredDiagnosisData.id")), "count"]
+        [Sequelize.fn("COUNT", Sequelize.col("DiagnosisData.id")), "count"]
       ],
       include: [{
         model: DiagnosisData,
-        as: "RegisteredDiagnosisData",
         attributes: []
       }],
       group: ['nameKr']
@@ -34,31 +33,136 @@ const showDiagnosisSummary = async (req, res, next) => {
     // '0-19살', '20-29살', '30-30살', '40-49살', '50-59살', '60-69살', '70살 이상'
     const currentYear = new Date().getFullYear();
     const birthYear19 = new Date(((currentYear + 1) - 19).toString());
+    const birthYear29 = new Date(((currentYear + 1) - 29).toString());
+    const birthYear39 = new Date(((currentYear + 1) - 39).toString());
+    const birthYear49 = new Date(((currentYear + 1) - 49).toString());
+    const birthYear59 = new Date(((currentYear + 1) - 59).toString());
+    const birthYear69 = new Date(((currentYear + 1) - 69).toString());
 
-    const val_2_1 = await DiagnosisData.findAll({
-      attributes: [],
+    const val_2_1 = await DiagnosisData.findAndCountAll({
+      attributes: ['id'],
       where: { fkDiagnosisId: diagnosisId },
       include: [{
         model: User,
-        as: 'RegisteringDiagnosisData',
-        // attributes: [],
+        attributes: ['id'],
+        required: true,  // inner join(null인 경우 포함되지 않음, profile의 where과 합쳐져서 where birthDate 조건에 맞지 않는 (profile을 가지는) user는 포함되지 않는다.)
         include: [{
           model: Profile,
-          as: 'Profile',
-          // attributes: [],
-          where: { birthDate: {
-            [Op.lte]: birthYear19  // <= 19(살)
-          }},
-          required: false
+          attributes: ['id'],
+          where: { birthDate: {  // where clause - 자동으로 'required: true' -> inner join(null인 경우 포함되지 않음)
+            [Op.gte]: birthYear19  // <= 19(살) (>= YYYY-01-01)
+          }}
         }]
       }]
     });
-    console.log(JSON.stringify(val_2_1));
-    
+     
     chartData["ageArr"] = [];
-    chartData["ageArr"].push(val_2_1[0].count);
+    chartData["ageArr"].push(val_2_1.count);
     console.log(chartData);
 
+    const val_2_2 = await DiagnosisData.findAndCountAll({
+      attributes: ['id'],
+      where: { fkDiagnosisId: diagnosisId },
+      include: [{
+        model: User,
+        attributes: ['id'],
+        required: true,  // inner join(null인 경우 포함되지 않음, profile의 where과 합쳐져서 where birthDate 조건에 맞지 않는 (profile을 가지는) user는 포함되지 않는다.)
+        include: [{
+          model: Profile,
+          attributes: ['id'],
+          where: { birthDate: {  // where clause - 자동으로 'required: true' -> inner join(null인 경우 포함되지 않음)
+            [Op.lt]: birthYear19,  //  > 19(살) (< YYYY-01-01)
+            [Op.gte]: birthYear29  // <= 29(살) (>= YYYY-01-01)
+          }}
+        }]
+      }]
+    });
+     
+    chartData["ageArr"].push(val_2_2.count);
+    console.log(chartData);
+
+    const val_2_3 = await DiagnosisData.findAndCountAll({
+      attributes: ['id'],
+      where: { fkDiagnosisId: diagnosisId },
+      include: [{
+        model: User,
+        attributes: ['id'],
+        required: true,  // inner join(null인 경우 포함되지 않음, profile의 where과 합쳐져서 where birthDate 조건에 맞지 않는 (profile을 가지는) user는 포함되지 않는다.)
+        include: [{
+          model: Profile,
+          attributes: ['id'],
+          where: { birthDate: {  // where clause - 자동으로 'required: true' -> inner join(null인 경우 포함되지 않음)
+            [Op.lt]: birthYear39,  //  > 39(살) (< YYYY-01-01)
+            [Op.gte]: birthYear49  // <= 49(살) (>= YYYY-01-01)
+          }}
+        }]
+      }]
+    });
+     
+    chartData["ageArr"].push(val_2_3.count);
+    console.log(chartData);
+
+    const val_2_4 = await DiagnosisData.findAndCountAll({
+      attributes: ['id'],
+      where: { fkDiagnosisId: diagnosisId },
+      include: [{
+        model: User,
+        attributes: ['id'],
+        required: true,  // inner join(null인 경우 포함되지 않음, profile의 where과 합쳐져서 where birthDate 조건에 맞지 않는 (profile을 가지는) user는 포함되지 않는다.)
+        include: [{
+          model: Profile,
+          attributes: ['id'],
+          where: { birthDate: {  // where clause - 자동으로 'required: true' -> inner join(null인 경우 포함되지 않음)
+            [Op.lt]: birthYear49,  //  > 49(살) (< YYYY-01-01)
+            [Op.gte]: birthYear59  // <= 59(살) (>= YYYY-01-01)
+          }}
+        }]
+      }]
+    });
+
+    chartData["ageArr"].push(val_2_4.count);
+    console.log(chartData);
+
+    const val_2_5 = await DiagnosisData.findAndCountAll({
+      attributes: ['id'],
+      where: { fkDiagnosisId: diagnosisId },
+      include: [{
+        model: User,
+        attributes: ['id'],
+        required: true,  // inner join(null인 경우 포함되지 않음, profile의 where과 합쳐져서 where birthDate 조건에 맞지 않는 (profile을 가지는) user는 포함되지 않는다.)
+        include: [{
+          model: Profile,
+          attributes: ['id'],
+          where: { birthDate: {  // where clause - 자동으로 'required: true' -> inner join(null인 경우 포함되지 않음)
+            [Op.lt]: birthYear59,  //  > 59(살) (< YYYY-01-01)
+            [Op.gte]: birthYear69  // <= 69(살) (>= YYYY-01-01)
+          }}
+        }]
+      }]
+    });
+     
+    chartData["ageArr"].push(val_2_5.count);
+    console.log(chartData);
+
+    const val_2_6 = await DiagnosisData.findAndCountAll({
+      attributes: ['id'],
+      where: { fkDiagnosisId: diagnosisId },
+      include: [{
+        model: User,
+        attributes: ['id'],
+        required: true,  // inner join(null인 경우 포함되지 않음, profile의 where과 합쳐져서 where birthDate 조건에 맞지 않는 (profile을 가지는) user는 포함되지 않는다.)
+        include: [{
+          model: Profile,
+          attributes: ['id'],
+          where: { birthDate: {  // where clause - 자동으로 'required: true' -> inner join(null인 경우 포함되지 않음)
+            [Op.lt]: birthYear69  //  > 69(살) (< YYYY-01-01)
+          }}
+        }]
+      }]
+    });
+     
+    chartData["ageArr"].push(val_2_6.count);
+    console.log(chartData);
 
     return res.status(200).json({ chartData });
   } catch (e) {
