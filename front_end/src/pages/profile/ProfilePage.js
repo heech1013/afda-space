e71431ca6a0e0dfe6dba732f530101016a620ef6
aiden shin <heech1013@gmail.com>
2020-03-03 22:06:38 +0900

    Fix: ProfilePage - MiniMenu position

--- front_end/src/pages/profile/ProfilePage.js
+++ front_end/src/pages/profile/ProfilePage.js
@@ -10,10 +10,10 @@ const ProfilePage = ({match}) => {
   const { id } = match.params;  // App.js에서 설정한 params 변수명: id
   return (
     <PageTemplate>
-      <ProfileCardContainer
-        userId={id}/>
       <MiniMenu 
         buttonArr={['프로필', '진단명', '증상', '처방약']}/>
+      <ProfileCardContainer
+        userId={id}/>
       {/* <PostListContainer
         userId={id}/> */}
       <ProfileUpdateModalContainer
