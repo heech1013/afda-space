bec5718db2defe020cd965830eea8329d0916d4e
Aiden Shin <heech1013@gmail.com>
2021-08-18 17:17:59 +0900

    ts migration: Button.tsx

--- front_end/src/components/common/Button/Button.tsx
+++ front_end/src/components/common/Button/Button.tsx
@@ -1,16 +1,31 @@
+import React, { FC } from 'react'
 import styles from './Button.scss';
 import classNames from 'classnames/bind';
 import { Link, withRouter } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
-const Div = ({children, ...rest}) => <div {...rest}>{children}</div>
+interface DivProp {
+  children: any;
+  rest?: any[];
+}
+interface ButtonProp {
+  children?: any;
+  theme?: string;
+  to?: any;
+  onClick?: any;
+  location?: any;
+}
 
-const Button = ({
+// const Div = ({ children,...rest }: DivProp): JSX.Element => <div {...rest}>{children}</div>
+const Div: FC<DivProp> = ({ children, ...rest }) => <div {...rest}>{children}</div>
+
+const Button: FC<ButtonProp> = ({
   children, theme = 'default', to, onClick, location
 }) => {
   const Element = to ? Link : Div;
   const clicked = to ? location.pathname.split('/')[1] === to.split('/')[1] : null;
+
   return (
     <Element
       className={cx('button', theme, { clicked })}
