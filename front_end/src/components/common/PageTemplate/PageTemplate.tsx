1effe68f836a49ee84e972af4d37e022226df7bc
Aiden Shin <heech1013@gmail.com>
2021-09-18 23:05:20 +0900

    typescript migration: PageTemplate.tsx

--- front_end/src/components/common/PageTemplate/PageTemplate.tsx
+++ front_end/src/components/common/PageTemplate/PageTemplate.tsx
@@ -4,15 +4,19 @@ import classNames from 'classnames/bind';
 
 import HeaderContainer from 'containers/common/HeaderContainer';
 
+interface PageTemplateProps {
+  children: React.ReactNode
+}
+
 const cx = classNames.bind(styles);
 
-const PageTemplate = ({children}) => (
-  <div className={cx('page-template')}>
+function PageTemplate({children}: PageTemplateProps) {
+  return <div className={cx('page-template')}>
     <HeaderContainer/>
     <main>
       {children}
     </main>
   </div>
-);
+};
 
 export default PageTemplate;
