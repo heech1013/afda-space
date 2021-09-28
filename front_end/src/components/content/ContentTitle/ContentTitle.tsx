2627f47b74134c1997e9e68b3475e8f37f93fb6d
Aiden Shin <heech1013@gmail.com>
2021-09-29 00:24:01 +0900

    typescript migration: ContentTitle.tsx

--- front_end/src/components/content/ContentTitle/ContentTitle.tsx
+++ front_end/src/components/content/ContentTitle/ContentTitle.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './ContentTitle.scss';
-import { withRouter } from 'react-router-dom';
-
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 import Button from 'components/common/Button';
+
+type ContentTitleProps = RouteComponentProps & {
+  nameKr: string;
+  nameEn: string;
+  onClick: () => void
+  buttonString: string;
+}
 
 const cx = classNames.bind(styles);
 
-const ContentTitle = ({ location, nameKr, nameEn, onClick, buttonString}) => {
+function ContentTitle({ location, nameKr, nameEn, onClick, buttonString }: ContentTitleProps) {
   const noTitleName = location.pathname.split('/')[1] === 'profile';  // 프로필 페이지: true
   const bracket = nameEn ? false: true;
   return (
